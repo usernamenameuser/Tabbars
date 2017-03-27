de1df229ad7fd75e184aef8518b243be71057ab6
usernamenameuser <writeme@email.ua>
2017-03-27 23:44:14 +0300

    Initial Commit

---
+++ Tabbars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tabbars
+//
+//  Created by user on 27.03.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tabbars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tabbars
+//
+//  Created by user on 27.03.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
